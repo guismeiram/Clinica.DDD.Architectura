66f553077c4d31957436f986a1cf7097a0a4925e
guismeiram <boralagui@outlook.com>
2022-12-16 23:32:23 -0300

    Commitando services

--- src/Clinica.DDD.Domain/Services/ServiceBase.cs
+++ src/Clinica.DDD.Domain/Services/ServiceBase.cs
@@ -1,61 +1,51 @@
 ﻿using Clinica.DDD.Core.DomainObjects;
+using Clinica.DDD.Domain.Interfaces.Repository;
 using Clinica.DDD.Domain.Interfaces.Services;
+
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Clinica.DDD.Domain.Services
 {
     public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : Entity, new()
     {
-        protected readonly ClinicaDbContext Db;
-        protected readonly DbSet<TEntity> DbSet;
+        private readonly IRepositoryBase<TEntity> _repository;
 
-        protected ServiceBase(ClinicaDbContext db)
+        public async Task Adicionar(TEntity entity)
         {
-            Db = db;
-            DbSet = db.Set<TEntity>();
+            await _repository.Adicionar(entity);
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            await _repository.Atualizar(entity);
         }
 
         public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
         {
-            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
-        }
+            return await _repository.Buscar(predicate);
+                //            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
 
-        public virtual async Task<TEntity> ObterPorId(string id)
-        {
-            return await DbSet.FindAsync(id);
-        }
-
-        public virtual async Task<List<TEntity>> ObterTodos()
-        {
-            return await DbSet.ToListAsync();
-        }
-
-        public virtual async Task Adicionar(TEntity entity)
-        {
-            DbSet.Add(entity);
-            await SaveChanges();
-        }
-
-        public virtual async Task Atualizar(TEntity entity)
-        {
-            DbSet.Update(entity);
-            await SaveChanges();
-        }
-
-        public virtual async Task Remover(string id)
-        {
-            DbSet.Remove(new TEntity { Id = id });
-            await SaveChanges();
-        }
-        public async Task<int> SaveChanges()
-        {
-            return await Db.SaveChangesAsync();
         }
 
         public void Dispose()
         {
-            Db?.Dispose();
+            _repository.Dispose();
         }
+
+        public Task<TEntity> ObterPorId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<TEntity>> ObterTodos() => await _repository.ObterTodos();
+
+        public async Task Remover(string id)
+        {
+            await _repository.Remover(id);
+        }
+
+        public async Task<int> SaveChanges() => await _repository.SaveChanges();
     }
 }
